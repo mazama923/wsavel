09c8e6abe70c7a1a808a7a8b9181aa07639ed51f
mazama923 <brasses_wearier0a@icloud.com>
2025-03-16 20:52:05 +0100

    add debug

--- internal/backup/backup.go
+++ internal/backup/backup.go
@@ -10,14 +10,20 @@ func BackupWSL(wslname, backupPath string, maxkeep, mindays int, compress bool) 
 		return "", err
 	}
 
+  fmt.Println("Check backup path passed")
+
 	numberOfDaysOfLastBackup, err := dateOfLastBackup(wslname, backupPath)
 	if err != nil {
 		return "", err
 	}
 
+  fmt.Println("Check date of last backup passed")
+
 	if numberOfDaysOfLastBackup < mindays {
 		return "", nil
 	}
+
+  fmt.Println("Check min days passed")
 
 	date := time.Now().Format("2006-01-02") // Format YYYY-MM-DD
 	backupFilePath := fmt.Sprintf("%s/%s-backup-%s.tar", backupPath, wslname, date)
@@ -25,11 +31,13 @@ func BackupWSL(wslname, backupPath string, maxkeep, mindays int, compress bool) 
 	if err := exportWSL(wslname, backupFilePath); err != nil {
 		return "", err
 	}
+  fmt.Println("Export WSL passed")
 
 	if compress {
 		if err := compressBackup(backupFilePath); err != nil {
 			return "", err
 		}
+		backupFilePath += ".gz"
 	}
 
 	return backupFilePath, nil
